260198af4efe7fae11ae45d99eea427d83b9031c
Agus Soedibjo <agus.soedibjo@thinkershq.com>
2014-03-31 00:45:32 +0100

    pod update

--- Example/Pods/Pods-environment.h
+++ Example/Pods/Pods-environment.h
@@ -9,18 +9,18 @@
 // ASOAnimatedButton
 #define COCOAPODS_POD_AVAILABLE_ASOAnimatedButton
 #define COCOAPODS_VERSION_MAJOR_ASOAnimatedButton 0
-#define COCOAPODS_VERSION_MINOR_ASOAnimatedButton 2
+#define COCOAPODS_VERSION_MINOR_ASOAnimatedButton 3
 #define COCOAPODS_VERSION_PATCH_ASOAnimatedButton 0
 
 // ASOAnimatedButton/BounceButton
 #define COCOAPODS_POD_AVAILABLE_ASOAnimatedButton_BounceButton
 #define COCOAPODS_VERSION_MAJOR_ASOAnimatedButton_BounceButton 0
-#define COCOAPODS_VERSION_MINOR_ASOAnimatedButton_BounceButton 2
+#define COCOAPODS_VERSION_MINOR_ASOAnimatedButton_BounceButton 3
 #define COCOAPODS_VERSION_PATCH_ASOAnimatedButton_BounceButton 0
 
 // ASOAnimatedButton/TwoStateButton
 #define COCOAPODS_POD_AVAILABLE_ASOAnimatedButton_TwoStateButton
 #define COCOAPODS_VERSION_MAJOR_ASOAnimatedButton_TwoStateButton 0
-#define COCOAPODS_VERSION_MINOR_ASOAnimatedButton_TwoStateButton 2
+#define COCOAPODS_VERSION_MINOR_ASOAnimatedButton_TwoStateButton 3
 #define COCOAPODS_VERSION_PATCH_ASOAnimatedButton_TwoStateButton 0
 
