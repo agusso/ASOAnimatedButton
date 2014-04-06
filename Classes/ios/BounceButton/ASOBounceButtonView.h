939a7f524deaff57c178e66fd6cd00f7963b3524
Agus Soedibjo <agus.soedibjo@thinkershq.com>
2014-04-06 17:26:05 +0100

    Add self documented comments - ready for version 1.0.1

--- Classes/ios/BounceButton/ASOBounceButtonView.h
+++ Classes/ios/BounceButton/ASOBounceButtonView.h
@@ -5,50 +5,114 @@
 //  Created by Agus Soedibjo on 8/2/14.
 //  Copyright (c) 2014 Agus Soedibjo. All rights reserved.
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import <UIKit/UIKit.h>
+/**
+ *  The ASOBounceButtonView class defines the view area and the interfaces for managing the speed, fade effect duration and bouncing distance all the bouncing buttons added on this view.
+ */
 
 @protocol ASOBounceButtonViewDelegate;
 
 @interface ASOBounceButtonView : UIView
 
-// Available animations style
+/**
+ *  The animation style applied to the buttons
+ *  'ASOAnimationStyleExpand': provides an effect of growing where buttons movement follow the path of other buttons defined in its previous order
+ *  'ASOAnimationStyleRiseProgressively': provides an effect where all defined buttons move directly from starting positon to its final position. The movement pace of each buttons are slower gradually
+ *  'ASOAnimationStyleRiseConcurrently': provides an effect where all defined buttons move directly from starting position to its final position at the same time
+ */
 typedef enum {
-    ASOAnimationStyleExpand, // ASOAnimationStyleExpand provides an effect of growing where buttons movement follow the path of other buttons defined in its previous order.
-    ASOAnimationStyleRiseProgressively, // ASOAnimationStyleRiseProgressively provides an effect where all defined buttons move directly from starting positon to its final position. The movement pace of each buttons are slower gradually.
-    ASOAnimationStyleRiseConcurrently // ASOAnimationStyleRiseConcurrently provides an effect where all defined buttons move directly from starting position to its final position at the same time.
+    ASOAnimationStyleExpand,
+    ASOAnimationStyleRiseProgressively,
+    ASOAnimationStyleRiseConcurrently
 }ASOAnimationStyle;
 
-// Control the speed of each animated button
+/**
+ *  Control the speed of each animated button.
+ *  A float value stored in NSNumber object. The bigger is the value, the slower is the bounce effect speed. Default value is '0.1'.
+ */
 @property (strong, nonatomic) NSNumber *speed;
 
-// Control the speed of fade-out effect when the view is collapsed
+/**
+ *  Control the fade-out effect duration between state changing.
+ *  A float value stored in NSNumber object. The bigger is the value, the longer is the duration. Default value is '0.1'.
+ */
 @property (strong, nonatomic) NSNumber *fadeOutDuration;
 
-// Control the bouncing distance of each animated button
+/**
+ *  Control the bouncing distance of each animated button.
+ *  A float value stored in NSNumber object. The bigger is the value, the further is the bouncing distance. Default value is '0.6'.
+ */
 @property (strong, nonatomic) NSNumber *bouncingDistance;
 
-// Read-only property (its value is privately calculated). This collapsedViewDuration gives the window for collapse animation to be shown before removing the bounce button view.
+/**
+ *  This collapsedViewDuration gives the window for collapse animation to be shown before removing the bounce button view.
+ *  Read-only property (its value is privately calculated)
+ */
 @property (strong, readonly, nonatomic) NSNumber *collapsedViewDuration;
 
-// Control which animation style to be used
+/**
+ *  Control which animation style to be applied to the buttons.
+ *  Three animation style as type of ASOAnimationStyle are available, They are 'ASOAnimationStyleExpand', ASOAnimationStyleRiseProgressively', and 'ASOAnimationStyleRiseConcurrently'.
+ */
 @property (nonatomic) ASOAnimationStyle animationStyle;
 
+/**
+ *  ASOBounceButtonView delegate accessor.
+ *  It is to invoke delegation method when each of bounce buttons defined in this view is tapped.
+ */
 @property (weak, nonatomic) id <ASOBounceButtonViewDelegate> delegate;
 
-// Set the frame value of the view which is used to launch this bounce button view via this method
+/**
+ *  Bounce button animation starting point
+ *
+ *  @param startingViewFrame Frame value of the view which is used to launch this bounce button view
+ */
 - (void) setAnimationStartFromHere:(CGRect)startingViewFrame;
 
-// Add individual bounce button
+/**
+ *  Add individual bounce button to this view
+ *
+ *  @param bounceButton A UIButton object to be animated in this bounce button view
+ */
 - (void) addBounceButton:(UIButton *)bounceButton;
 
-// Add all bounce buttons at once using an array
+/**
+ *  Add all bounce buttons defined in an array to this view
+ *
+ *  @param arrBounceButtons An array of UIButton object to be animated in this bounce button view
+ */
 - (void) addBounceButtons:(NSArray *)arrBounceButtons;
 
-// Launch this bounce button view
+/**
+ *  Launch this bounce button view
+ *
+ *  @param animationStyle ASOAnimationStyle used when launching this bounce button view
+ */
 - (void) expandWithAnimationStyle:(ASOAnimationStyle)animationStyle;
 
-// Remove this bounce button view from its parent view
+/**
+ *  Remove this bounce button view from its parent view
+ *
+ *  @param animationStyle ASOAnimationStyle used when removing this bounce button view
+ */
 - (void) collapseWithAnimationStyle:(ASOAnimationStyle)animationStyle;
 
 @end
--- Classes/ios/TwoStateButton/ASOTwoStateButton.h
+++ Classes/ios/TwoStateButton/ASOTwoStateButton.h
@@ -5,32 +5,79 @@
 //  Created by Agus Soedibjo on 4/2/14.
 //  Copyright (c) 2014 Agus Soedibjo. All rights reserved.
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import <UIKit/UIKit.h>
 
 @interface ASOTwoStateButton : UIButton
 
-// Store image name for one of the state
+/**
+ *  Store image name for one of the state.
+ *  The name of image stored in Images.xcassets.
+ */
 @property (strong, nonatomic) NSString *onStateImageName;
 
-// Store image name for the other state
+/**
+ *  Store image name for the other state.
+ *  The name of image stored in Images.xcassets.
+ */
 @property (strong, nonatomic) NSString *offStateImageName;
 
-// Control whether fade effect is enabled between state changing
+/**
+ *  Control whether fade effect is enabled between state changing.
+ *  A Boolean value, set 'YES' to enable the fade effect.
+ */
 @property (nonatomic) BOOL isFadeEffectEnabled;
 
-// Control the effect duration between state changing
+/**
+ *  Control the effect duration between state changing.
+ *  Default value is '0.6'.
+ */
 @property (nonatomic) NSTimeInterval effectDuration;
 
-// Toggle value between state
+/**
+ *  A Boolean value between state (read-only).
+ *  This value is to be used to indicate what is the current state.
+ *
+ */
 @property (readonly, nonatomic) BOOL isOn;
 
+/**
+ *  Initialise all the required property to animate the button.
+ *
+ *  @param fadeEnabled Set 'YES' to enable the fade effect between state changing.
+ */
 - (void)initAnimationWithFadeEffectEnabled:(BOOL)fadeEnabled;
 
-// Insert custom view underneath this ASOTwoStateButton view
+/**
+ *  A feature to insert a custom UIView
+ *
+ *  @param customView UIView object to be added underneath this ASOTwoStateButton view.
+ */
 - (void)addCustomView:(UIView *)customView;
 
-// Remove the custom view added by addCustomView method
+/**
+ *  Remove the custom UIView added by 'addCustomView' method
+ *
+ *  @param customView     UIView object added by 'addCustomView' method
+ *  @param delayInSeconds Set delay to show animation on the customView to be completed before removing this custom UIView
+ */
 - (void)removeCustomView:(UIView *)customView interval:(double)delayInSeconds;
 
 @end
